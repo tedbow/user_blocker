56a014bd7dff552caaf1f34768818825fa67f507
Ted Bowman <ted.git@tedbow.com>
2016-01-26 10:51:57 -0500

    init add

---
+++ src/Form/BlockerForm.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\user_blocker\Form\BlockerForm.
+ */
+
+namespace Drupal\user_blocker\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\user\Entity\User;
+
+/**
+ * Form to allow users to block other users
+ */
+class BlockerForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'blocker_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['username'] = [
+      '#type' => 'textfield',
+      '#required' => TRUE,
+      '#title' => $this->t('Username'),
+      '#description' => $this->t('Enter the username of the user you want to block'),
+      '#maxlength' => 64,
+      '#size' => 64,
+    ];
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Block User'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $username = $form_state->getValue('username');
+    /** @var User $account */
+    if ($user = user_load_by_name($username)) {
+      $user->block();
+    }
+    else {
+      drupal_set_message($this->t('User @username was not found.', ['@username' => $username]), 'warning');
+    }
+  }
+
+}
